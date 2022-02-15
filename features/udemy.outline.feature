1bf6cb1f3afdf64ea13dc628b0c8a060a853bd89
Manuel Abruzzo <manuel.abruzzo@patagonian.it>
2022-02-15 09:41:41 -0300

    Added: course filters

--- features/udemy.outline.feature
+++ features/udemy.outline.feature
@@ -9,7 +9,7 @@ Feature: Enroll in a Udemy course
     Given I log in the website
 
   Scenario Outline: Enroll on course
-    Given I select a category
-    And I apply the filters for "free" and "english" courses
+    Given I select a category on the homescreen
+    And I apply the filters for free and english courses
     When I select the second result and enroll in the course
 # Then Start to type your Then step here
--- src/steps/udemyPage.steps.ts
+++ src/steps/udemyPage.steps.ts
@@ -7,16 +7,17 @@ require('dotenv').config();
 
 Given('I log in the website', async function (this: ICustomWorld) {
   const { page } = this;
-  if (!page) throw new Error('No page awailable');
+  if (!page) throw new Error('No page available');
 
   //TODO: validate defined BASE_URL
   navigate(page, process.env.BASE_URL || '');
 
   await Promise.all([
-    await page.waitForNavigation(),
+    await page.waitForNavigation({ waitUntil: 'load' }),
     await page.locator('a:has-text("Log in")').click(),
   ]);
 
+  await page.waitForSelector('.social-btns--container');
   const email = page.locator(`[name = 'email']`);
   const password = page.locator(`[name = 'password']`);
 
@@ -26,5 +27,29 @@ Given('I log in the website', async function (this: ICustomWorld) {
   await password.fill(process.env.PASSWORD || '');
 
   await page.locator(`[name="submit"]`).click();
-  expect(await page.title()).toContain('Online Courses');
+  await page.waitForNavigation({ waitUntil: 'load' });
+});
+
+Given('I select a category on the homescreen', async function (this: ICustomWorld) {
+  const { page } = this;
+  if (!page) throw new Error('No page available');
+
+  await page.locator(`button:has-text('Categories')`).hover();
+  const dropmenu = page.locator('.js-browse-nav-level-one');
+  await dropmenu.locator(`div:has-text('Development')`).first().click();
+
+  await page.waitForNavigation({ waitUntil: 'load' });
+  expect(page.url()).toContain('courses/development');
+});
+
+Given('I apply the filters for free and english courses', async function (this: ICustomWorld) {
+  const { page } = this;
+  if (!page) throw new Error('No page available');
+
+  const filters = page.locator('#filter-form');
+  await filters.locator(`button:has-text('Language')`).click();
+  await filters.locator(`label:has-text('English')`).click();
+  await filters.locator(`button:has-text('Price')`).click();
+  await filters.locator(`label:has-text('Free')`).click();
+  await page.waitForNavigation({ waitUntil: 'networkidle' });
 });
