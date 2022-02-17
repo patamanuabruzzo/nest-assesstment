21709daaa3df108eb5fbabe879e3e649074d9487
Manuel Abruzzo <manuel.abruzzo@patagonian.it>
2022-02-17 00:50:05 -0300

    Implemented full feature steps

--- src/steps/udemyPage.steps.ts
+++ src/steps/udemyPage.steps.ts
@@ -6,6 +6,8 @@ import { expect } from '@playwright/test';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
+let course: string;
+
 Given('I log in the website', async function (this: ICustomWorld) {
   const { page } = this;
   if (!page) throw new Error('No page available');
@@ -14,7 +16,7 @@ Given('I log in the website', async function (this: ICustomWorld) {
   navigate(page, process.env.BASE_URL || '');
 
   await Promise.all([
-    await page.waitForNavigation({ waitUntil: 'load' }),
+    await page.waitForSelector('a:has-text("Log in")'),
     await page.locator('a:has-text("Log in")').click(),
   ]);
 
@@ -28,7 +30,6 @@ Given('I log in the website', async function (this: ICustomWorld) {
   await password.fill(process.env.PASSWORD || '');
 
   await page.locator(`[name="submit"]`).click();
-  // await page.waitForNavigation({ waitUntil: 'load' });
   await page.waitForURL(process.env.BASE_URL || '');
 });
 
@@ -36,7 +37,6 @@ Given('I select a category on the homescreen', async function (this: ICustomWorl
   const { page } = this;
   if (!page) throw new Error('No page available');
 
-  //FIXME: sometimes, it misses the hover menu and get stuck
   await page.locator(`button:has-text('Categories')`).hover();
   const dropmenu = page.locator('.js-browse-nav-level-one');
   await dropmenu.locator(`div:has-text('Development')`).first().click();
@@ -61,28 +61,34 @@ When('I select the second result and enroll in the course', async function (this
   if (!page) throw new Error('No page available');
 
   const courses = page.locator('.course-list--container--3zXPS');
-  await courses.locator('.popper--popper--2r2To >> nth=5').click();
+  await courses.locator('.popper--popper--2r2To >> nth=1').click();
 
-  // await page.waitForNavigation({ waitUntil: 'load' });
-  await page.waitForURL(new RegExp('.*course/.*'));
-  expect(page.url()).toContain('/course/');
+  await page.waitForSelector('.heading');
 
   try {
     await page.waitForSelector(`.heading button:has-text("Enroll now")`, { timeout: 10000 });
-    await page.locator('.heading').locator(`button:has-text("Enroll now")`).click();
+    // await page.locator('.heading').locator(`button:has-text("Enroll now")`).click();
+    course = await page.locator('.heading h1').innerText();
   } catch (error) {
     throw new Error('Unable to enroll on this course');
   }
 });
 
-// When('I request a list of courses from the API', async function (this: ICustomWorld) {});
+When('I request a list of courses from the API', async function (this: ICustomWorld) {
+  const { api } = this;
+  if (!api?.context) throw new Error('No api context available');
 
-Then('The API returns a list of courses', async function (this: ICustomWorld) {
-  const response = await getCoursesList(this.api);
+  const response = await getCoursesList(api);
+  expect(response.ok()).toBeTruthy();
+  api.response = response;
+});
 
-  // eslint-disable-next-line no-console
-  console.log(await response.json());
+Then('the enrolled course should be in the list', async function (this: ICustomWorld) {
+  const { api } = this;
+  if (!api?.response) throw new Error('No api response found');
 
-  // eslint-disable-next-line no-console
-  console.log(response.status() + ' - ' + response.statusText());
+  const courses = (await api.response.json()).results;
+  const enrolled = courses.find((entry: { title: string }) => entry.title === course);
+
+  expect(enrolled).toBeTruthy();
 });
--- src/support/config.ts
+++ src/support/config.ts
@@ -2,6 +2,7 @@ import { LaunchOptions } from 'playwright';
 
 const browserOptions: LaunchOptions = {
   slowMo: 0,
+  headless: false,
   args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
   firefoxUserPrefs: {
     'media.navigator.streams.fake': true,
